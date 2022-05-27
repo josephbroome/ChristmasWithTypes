28fddaf1898d91d7990381fc0a12ce54263e0f02
Joseph Broome <josephlloydbroome@gmail.com>
2022-05-27 14:26:09 -0700

    changes

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -17,7 +17,7 @@ namespace ChristmasWithTypes
             xmas.Presents =  new string[3]{"xbox", "my little pony", "weapons of mass destruction" }; 
 
             //TODO Set the TreeHeight to 10
-            xmas.TreeHeight = null;
+            xmas.TreeHeight = 10;
 
             Console.WriteLine($"This year christmas falls on {xmasDay} \n");
             Console.WriteLine($"Our tree will be {xmas.TreeHeight} feet high \n");
